9162a680809208923f16d9b80c6af9978b1ed146
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-10-13 18:19:43 +0000

    adding 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- *  Return: 0 if exited properly, non-zero otherwise
- *  
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
  */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
+
