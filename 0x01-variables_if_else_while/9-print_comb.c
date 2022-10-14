0f4b67e113a85d3164af8c4f3385f8ce26a37812
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2022-10-15 00:17:14 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,15 @@
-t_comb.c
-248 bytes
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Succes)
  */
+
 int main(void)
 {
 	int n;
+
 	for (n = 0 ; n < 10 ; n++)
 	{
 		putchar(n + '0');
