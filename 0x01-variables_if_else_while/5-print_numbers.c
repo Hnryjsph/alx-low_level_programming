ead75ddbee917195434f1e6afe592ca7ab1e9ebf
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2022-10-15 00:03:18 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Succes)
-*/
-
+ * main - prints all single digit numbers of base
+ * 10 starting from 0, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
-	for (n = 0 ; n < 10 ; n++)
+	for (n = 0; n < 10; n++)
 		printf("%d", n);
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
