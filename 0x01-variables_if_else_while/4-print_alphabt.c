81ec85d8fbfaf3c83afab047ffa6830ba683b5c3
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-10-14 20:47:18 +0000

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*
+* Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+		char c;
+
+			for (c = 'a' ; c <= 'z' ; c++)
+					{
+								if ((c == 'q' || c == 'e') != 1)
+											{
+															putchar(c);
+																	}
+
+									}
+				putchar('\n');
+					return (0);
+}
+
+
