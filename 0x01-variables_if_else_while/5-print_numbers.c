c809e1cd21038a4cd90850df0c00da71de07c065
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2022-10-15 00:11:52 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,12 +5,17 @@
  * 10 starting from 0, followed by a new line
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
 
 	for (n = 0; n < 10; n++)
+	{
 		printf("%d", n);
+	}
+	
 	printf("\n");
+	
 	return (0);
 }
