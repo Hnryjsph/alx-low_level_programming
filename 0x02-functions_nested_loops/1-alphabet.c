ec8e89a0cc081a213a6588f00cc92fbe6187704f
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-10-18 18:46:42 +0000

    adding 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h";
+
+/**
+ * print_alphabet - prints the alphabet, in lowercas, follwed
+ */
+
+void printt_alphabet(void)
+{
+	char ch;
+	char 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
+
