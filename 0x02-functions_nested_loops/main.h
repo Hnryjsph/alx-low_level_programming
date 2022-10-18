f734fd9abcea7680098708500306bd86ad6a5513
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-10-18 18:19:26 +0000

    adding main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+/**
+ *  * print_alphabt - print the lower-case alphabet on one line.
+ *   */
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(char c);
+int _isalpha(char c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+
