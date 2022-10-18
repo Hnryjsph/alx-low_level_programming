6893bb53631b01647b0a641ebd816fb8ee93eafb
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-10-18 19:04:26 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+include "main.h"
+/**
+ * print_alphabet_x10 - prints alphatbet 10 time
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+	 	ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++
+	}
+}
