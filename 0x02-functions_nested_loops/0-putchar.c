065a20bb7135886284f4ac412470d2168eea65a0
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2022-10-18 21:29:49 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,14 @@
 
 int main(void){
 	_putchar('H');
-		_putchar('o');
-			_putchar('l');
-				_putchar('b');
-					_putchar('e');
-						_putchar('r');
-							_putchar('t');
-								_putchar('o');
-									_putchar('n');
-_putchar('\n');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
-
-
 }
