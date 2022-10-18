6fbb9db8717121cae9d006c5d804eb629c53d92f
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-10-18 18:41:13 +0000

    adding 0-putchar,c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- *  main: prints _putchar
- *  Return: Always 0 (Success)
- */
+*  main - prints _putchar
+*  Return: Always 0 (Success)
+*/
 
-int main(void){
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
