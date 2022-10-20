b053415d1359693489bd39d3317df1c34475241a
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-10-20 20:32:18 +0000

    adding

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,5 +5,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
