0b4e842d7fd6b37039aacefab07bb76bcf0d6e2e
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2022-10-21 00:35:07 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
+/**
+*FUNCTIONS
+*/
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
-
-#endif /* MAIN_H */
