5fbbc4bc1711912bb1f69b87afb29dc8e4fee9fd
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-10-20 18:12:05 +0000

    hot fix

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
+/**
+ *  positive_or_negative: returns whether a number is positive or negative
+ */
 
 void positive_or_negative(int i);
-
-#endif /* MAIN_H*/
