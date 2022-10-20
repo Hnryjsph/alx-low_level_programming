e6fe1a382a18d2b50de83aa7d3cb616652a64518
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2022-10-21 00:41:05 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,19 +5,19 @@
 * Return: 0
 */
 
-int main(void)
-{
-	int i;
+	int main(void)
+	{
+		int i;
 
-	printf("Infinite loop incoming :(\n");
-	
-	i = 0;
-	
-	/* while (i < 10) */
-	/*{*/
+		printf("Infinite loop incoming :(\n");
+
+		i = 0;
+
+		/* while (i < 10) */
+		/*{*/
 		/*putchar(i);*/
-	/*}*/
-	
+		/*}*/
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
