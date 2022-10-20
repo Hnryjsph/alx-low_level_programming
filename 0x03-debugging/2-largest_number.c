0096697f8a12be439863b3b01ffb50935b99f458
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-10-20 20:10:44 +0000

    adding 2-largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,14 +12,14 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
-	}else if (c > a && c > b)
+	}else if (c >= a && c >= b)
 	{
 		largest = c;
 	}
