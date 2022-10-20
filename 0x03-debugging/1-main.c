4b8511475c899a682132cb2bc527595fde607a9e
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-10-20 18:56:56 +0000

    quick fix

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,14 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-	/*
+	
 	i = 0;
 	
-	while (i < 10)
-	{
-		putchar(i);
-	}
-	*/
+	//while (i < 10)
+	//{
+	//	putchar(i);
+	//}
+	
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
