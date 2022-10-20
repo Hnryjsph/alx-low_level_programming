3d745cd2b6a90549a5b2318f3fe8cf844cebf882
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-10-20 20:19:21 +0000

    adding file

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 	
 	i = 0;
 	
-	//while (i < 10)
-	//{
-	//	putchar(i);
-	//}
+	/* while (i < 10) */
+	/*{*/
+		/*putchar(i);*/
+	/*}*/
 	
 	printf("Infinite loop avoided! \\o/\n");
 
