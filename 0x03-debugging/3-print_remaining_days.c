05d2b599ddae5e40445a2bafc857489f42dac4cf
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-10-20 20:55:06 +0000

    adding

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -26,7 +26,8 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
 		}
 		else
 		{
