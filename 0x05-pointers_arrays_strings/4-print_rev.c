907d65860f5cf74236403093ad370685b70d4134
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-10-24 18:35:12 +0000

    adding print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the reverse of a string
+ * @s: pointer that contains the refrence to tthe string
+ * @length: is the length of the string
+ * Return: Void
+ */
+
+void print_rev(char *s)
+{
+	int i,length;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	
+	length = i - 1;
+
+	while (length >= 0)
+	{
+		_putchar(str[length]);
+
+		length--;
+	}
+
+	_putchar('\n');
+
+}
