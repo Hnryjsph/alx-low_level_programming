061a0223324886e926938afafd6a15b3978aee3d
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-10-24 18:56:30 +0000

    adding rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: a pointer to the address of the string 
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char tmp;
+	int length, i, len1;
+
+	length = 0;
+	len1 = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len--] = tmp;
+	}
+}
