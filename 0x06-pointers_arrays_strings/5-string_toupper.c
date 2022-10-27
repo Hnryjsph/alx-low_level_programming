602367c1e2a5cd33377e42733a22267b607f3aed
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2022-10-27 21:18:54 +0300

    Update 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of string
+ * to uppercase
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+
+	return (s);
+}
