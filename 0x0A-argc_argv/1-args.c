b17091df9ba053d14cee8d0788c598df38693b6c
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-11-04 17:33:58 +0000

    adding a program that returns the number of arguments passed into the program

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,9 +9,9 @@
  * Return: 0
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc , char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
