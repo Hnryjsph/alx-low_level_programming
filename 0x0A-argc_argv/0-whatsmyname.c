76118fde77a2902beb7151614eb4fdb212645ced
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-11-04 17:24:48 +0000

    adding main.h

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
