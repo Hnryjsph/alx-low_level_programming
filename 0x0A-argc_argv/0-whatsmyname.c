54c0306d2a3889241df0eb2216d58b8103f7ef34
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-11-04 17:16:46 +0000

    adding files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+
+	return (0);
+}
