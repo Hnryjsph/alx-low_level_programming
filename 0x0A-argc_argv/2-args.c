395830591a776e0553dccc18c6ae67ce578c4ac5
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-11-04 17:43:40 +0000

    aDDING 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,13 +9,13 @@
  * Return: 0
  */
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for(i = 0; i < argc; i++)
 	{
-		printf("%s", argv[i]);
+		printf("%s\n", argv[i]);
 	}
 
 	return (0);
