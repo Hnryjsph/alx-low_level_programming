366c6054717d2df2ca8ff74905124dc9b78fead9
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-11-04 18:10:28 +0000

    adding 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,6 +9,56 @@
  * Return: 0
  */
 
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+	{
+		len ++;
+	}
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+		{
+			++d;
+		}
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+			{
+				digit = -digit;
+			}
+
+			n = n * 10 + digit;
+			f = 1;
+
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				break;
+			}
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+	{
+		return (0);
+	}
+	
+	return (n);
+}
+
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	if (sizeof(argv) > 1)
@@ -17,11 +67,13 @@ int main(int argc __attribute__((unused)), char *argv[])
 		int b;
 		int c;
 
-		a = argv[1];
-		b = argv[2];
+		a = _atoi(argv[1]);
+		b = _atoi(argv[2]);
 		c = a * b;
 		
 		printf("%d\n", c);
+
+		return (0);
 	}
 	else
 	{
