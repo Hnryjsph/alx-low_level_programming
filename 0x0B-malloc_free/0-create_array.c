7f42531f8c6414b0cf3242346b5b17c0f285c69a
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-11-07 17:41:37 +0000

    adding files

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,19 +13,23 @@ char *create_array(unsigned int size, char c)
 {
 	char *ptr;
 	unsigned int x;
-	
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
 	ptr = (char *) malloc(size);
-
+	
+	if (p == NULL)
+	{
+		return (0);
+	}
 	for (x = 0; x < size; x++)
 	{
 		*(ptr + x) = c;
 	}
-	
+
 	*(ptr + 1) = '\0';
 
 	return (ptr);
