30d3b87ba950a8c0e7f56b21d43c247c8f1c79d3
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-11-14 20:07:32 +0000

    hot fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dogs basic info
+ * @name: First Member
+ * @age: Second Member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+
 struct dog
 {
 	char *name;
