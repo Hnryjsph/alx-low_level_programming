6dcd2548eb97dd04399f963c2ec1c633942e16e6
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-11-14 20:16:44 +0000

    hot fix

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * init_dog - intializes the variable
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
