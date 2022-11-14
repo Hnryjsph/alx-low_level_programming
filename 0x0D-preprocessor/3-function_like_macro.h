2dc785c61387c24f9338b3d14b3c9cd674ef8dc4
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-11-14 19:31:34 +0000

    adding function like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,6 @@
-#ifndef LIKE
-#define LIKE
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < (0) ? -(x) : (x))	
-
+#define ABS(x) ((x) < (0) ? (x * -1) : (x))	
 
 #endif
