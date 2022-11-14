b1bb0bfe354e8707ca8882ef06470c2b6f2d18df
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-11-14 20:20:11 +0000

    hot fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - intializes the variable
@@ -8,6 +9,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+	}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
