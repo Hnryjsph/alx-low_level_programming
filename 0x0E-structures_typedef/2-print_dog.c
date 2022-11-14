34b438b98dc8c29db5d894a1a0c85b5905b855cf
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2022-11-14 23:36:10 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,18 +3,18 @@
 #include "dog.h"
 
 /**
- *  * print_dog - prints a struct dog
- *   * @d: struct dog to print
- *    */
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
+ */
 void print_dog(struct dog *d)
 {
-		if (d == NULL)
-					return;
+	if (d == NULL)
+		return;
 
-			if (d->name == NULL)
-						d->name = "(nil)";
-				if (d->owner == NULL)
-							d->owner = "(nil)";
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 
-					printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
