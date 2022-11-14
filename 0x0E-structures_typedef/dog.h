46b4daf9d20f2e49e0512f45caf3a17b732354d6
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2022-11-14 23:10:45 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,15 +2,13 @@
 #define DOG_H
 
 /**
- * struct dog - a dogs basic info
- * @name: First Member
- * @age: Second Member
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
  * @owner: Third member
  *
  * Description: Longer description
  */
-
-
 struct dog
 {
 	char *name;
@@ -19,9 +17,15 @@ struct dog
 };
 
 /**
- * dog_t - type for struct dog
+ * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
