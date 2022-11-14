ae7b88929754138b93299ebdfec8d0f325c5fd60
Henry Joseph Phillip Lokuku <hnryjsph@outlook.com>
2022-11-14 19:38:00 +0000

    hot fix

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/**
- * main - prints the file name 
- *
- * Return: 0
- */
 
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+ 
 int main(void)
 {
 	printf("%s\n", __FILE__);
