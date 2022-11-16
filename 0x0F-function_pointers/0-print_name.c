058fe942ee527f086d0a80ff45eb5ca057cdeb80
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2022-11-16 14:03:50 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,18 +2,13 @@
 #include "function_pointers.h"
 
 /**
-* print_name - prints the name of the passed variable
-* @name: string of name
-*
-* Return: void
-*/
-
-void print_name(char *name, void(*f)(char *))
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
+ */
+void print_name(char *name, void (*f)(char *))
 {
-    if (!name || !f)
-    {
-        return;
-    }
-
-    (*f)(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
