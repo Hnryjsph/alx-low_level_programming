e75ee8d8067c42a293158f816f18722b2f277f1d
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2022-11-17 22:21:18 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,20 +8,22 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
-	int sum = 0;
-	va_list list;
-	
 	if (n == 0)
 	{
 		return (0);
 	}
-
+	
+	unsigned int i;
+	int sum = 0;
+	va_list list;
+	
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
+	{
 		sum += va_arg(list, int);
-
+	}
+	
 	va_end(list);
 
 	return (sum);
