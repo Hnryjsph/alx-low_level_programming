0d9c6fb9da9e753baeab07392cba5a759178e4d0
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2022-12-01 23:06:13 +0300

    Update 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
+ *
+ * Return: value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_val;
+
+	if (index > 63)
+		return (-1);
+
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
+}
