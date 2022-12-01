94541140e36b44a0c5e5df2e09cb20d451ec514c
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2022-12-01 23:05:23 +0300

    Update 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks if a machine is little or big endian
+ * Return: 0 for big, 1 for little
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
